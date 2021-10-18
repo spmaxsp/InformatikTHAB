33bda9bd5ab732757cdf1b6a2e0d1b337024443b
spmaxsp <75380717+spmaxsp@users.noreply.github.com>
2021-10-18 08:13:16 +0200

    Übungstunde 18.10.2021

--- et1/U2/v1/main.c
+++ et1/U2/v1/main.c
@@ -1,7 +1,22 @@
 #include<stdio.h> 
+
+#define pi 3.14159
  
 void main() { 
-    float a = 3.1415;
-    printf("%f", a); 
+    float h;
+    float r;
+    float A;
+    float V;
+
+    printf("Radius: "); 
+    scanf("%f", &r);
+
+    printf("Hoehe: "); 
+    scanf("%f", &h);
+
+    V = pi * r * r * h;
+    A = 2 * pi * r * (r + h);
+    printf("Volumen: %f \n", V); 
+    printf("Oberflaeche: %f \n", A); 
     return;
 }
