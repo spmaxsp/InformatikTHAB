267dd4c0341def9d6aa84de1faa27449573b0d31
spmaxsp <75380717+spmaxsp@users.noreply.github.com>
2022-05-12 13:47:27 +0200

    Übung 4 fertig

--- et2/U4_v1/main.c
+++ et2/U4_v1/main.c
@@ -1,33 +1,103 @@
 #include<stdio.h>
-#include<string.h>  
-#include "myStruct.h"
-#include "myList.h"
+#include<stdlib.h>
+#include<string.h>
+#include "myList.h"  
 
-void printList(struct myList* p){
-    printf("Number: %d\n", p->value);
-    if (p->next != NULL) {
-        printList(p->next);
+int addToTail(myList** p_current, int value){
+    if (*p_current == NULL){              // überprüfe ob head bereits auf null zeigt
+        *p_current = (myList*) malloc(sizeof(myList));  //erstelle das head element
+        (*p_current)->next = NULL;
+        (*p_current)->value = value;
     }
-    return;
+    else {
+        myList* current = *p_current;     //current ist nun die adresse auf die head zeigt
+        while(current->next != NULL){     //solange != Null also ende der Lsiste
+            current = current->next;      //current wird das nächste element zugewisen
+        }
+        current->next = (myList*) malloc(sizeof(myList));  //alociere neuen speicher und schreibe die adresse un curent-next
+        if (current->next == NULL){          //überprüfe ob alocation erfolgreich
+            return -1;
+        }
+        current = current->next;             //curretn auf den neu erstellten Eintrag
+        current->value = value;
+    }
+    return 0;
 }
 
-void main() {
-    myList* head;
+int removeFromTail(myList** p_current){   // *p_current wäre der head-pointer in der main
+    if (*p_current == NULL){              // überprüfe ob head bereits auf head zeigt
 
-    malloc(sizeof(myList))
-    struct myList x;
-    x.value = 2;
-    x.next = NULL;
+    }
+    else if ((*p_current)->next == NULL){ //überprüfe ob der next des headpointer bereits auf NULL zeigt
+        free(*p_current);                 //wenn ja löche und setze auf null
+        *p_current = NULL;  
+    }
+    else{
+        myList* current = *p_current;     //current ist nun die adresse auf die head zeigt
+        myList* previous;
+        while(current->next != NULL){
+            previous = current;           //merke das vorherige element
+            current = current->next;      //springe zum nächsten
+        }
+        previous->next = NULL;            //das next des vorherigen auf null
+        free(current);                    //öoche das aktuelle element
+    }
+    return 0;
+}
+
+int printList(myList** p_current){
+    if (*p_current == NULL){              // überprüfe ob head bereits auf null zeigt
+
+    }
+    else {
+        myList* current = *p_current;     //current ist nun die adresse auf die head zeigt
+        printf("value: %d\n", current->value);
+        while(current->next != NULL){
+            current = current->next;      //gehe zum nächsten element und printe
+            printf("value: %d\n", current->value);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+int main() {
+    myList* head = NULL;
+
+    if(addToTail(&head, 11) == 1) {
+        printf("Konnte Element nicht hinzufügen!\n");
+        return -1;
+    }
+
+    printList(&head);
+
+    if(addToTail(&head, 22) == 1) {
+        printf("Konnte Element nicht hinzufügen!\n");
+        return -1;
+    }
+
+    printList(&head);
+
+    removeFromTail(&head);
+
+    printList(&head);
+
+    removeFromTail(&head);
+    removeFromTail(&head);
+
+    printList(&head);
+
+    if(addToTail(&head, 23) == 1) {
+        printf("Konnte Element nicht hinzufügen!\n");
+        return -1;
+    }
+
+    printList(&head);
+
+    removeFromTail(&head); //doppel-pointer notwendig da head nur auf den speicherplatz zeigt und head selbst nicht verändert weren kann
     
-    struct myList y;
-    y.value = 1;
-    y.next = &x;
-    
-    struct myList z;
-    z.value = 0;
-    z.next = &y;
-
-    printList(&z);
-
-    return;
+    return 0;
 }
